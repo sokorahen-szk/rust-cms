1c9c25e9670d3045329d382385d85a57bbed0245
sokorahen-szk <kamonotakahashi@gmail.com>
2023-06-28 23:03:55 +0900

    Fix: migration file

--- src/database/migrations/2023_01_22_121339_create_tags_table.php
+++ src/database/migrations/2023_01_22_121339_create_tags_table.php
@@ -16,6 +16,17 @@ return new class () extends Migration {
             $table->uuid("id", 36)->primary()->comment("タグID");
             $table->string("name")->comment("タグ名");
             $table->string("description")->comment("備考");
+            // タグを使用するターゲットについては、現時点では、
+            // players | clan
+            //       0 | 0　　　・・・どちらでも使用しない 2(00) = 10(0)
+            //       0 | 1     ・・・クランのみで使用 2(01) = 10(1)
+            //       1 | 0     ・・・プレイヤーのみで使用 2(10) = 10(2)
+            //       1 | 1     ・・・クラン、プレイヤーで使用 2(11) = 10(3)
+            // 仕様上、どちらでも使用しない事は想定しないため、2(00)は起こり得ない。
+            // 2(00)が起こる場合はバリデーションエラー等でユーザリクエスト時に弾く必要がある。
+            // その辺りの実装については、ドメイン層かプレゼンテーション層で対応する。
+            $table->integer("target_domain_permission")->comment("タグを使用するターゲット");
+            $table->boolean("is_enabled")->comment("タグ有効有無");
         });
     }
 
